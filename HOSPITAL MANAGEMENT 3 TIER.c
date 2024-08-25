9ea956ebec5adae10fe1d5888efe2bc21b003e27
Rohit-Raj01 <rajbussinesscontact@gmail.com>
2024-08-25 19:17:39 +0530

    Add files via upload

---
+++ HOSPITAL MANAGEMENT 3 TIER.c
@@ -0,0 +1,30 @@
+/********************
+// PROJECT HOSPITAL MANAGEMENT
+//********************
+//********************
+// INCLUDED HEADER FILES
+//********************
+#include <fstream.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <dos.h> 
+#include <ctype.h>
+ofstream 
+print("PRN"); void 
+print_title(); void 
+print_dashline(); void 
+print_date(); void 
+print_time();
+////////////////////////
+/ void title(); 
+void dashline(); 
+void date(); void 
+time();
+void print_dashline()
+{ for(int a=0;a<80;a++) 
+print<<'-';
+}
+void print_time()
+{
